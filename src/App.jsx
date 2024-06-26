f09f8c88ef64deedb98788e52f4ca3c7e074eb45
Andres Parra <byandrev@gmail.com>
2024-06-26 17:16:37 -0500

    feat: create login view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Switch } from 'wouter'
 
 import HomePage from './pages/Home'
 import Header from "./components/Header.jsx";
+import LoginPage from "./pages/Login.jsx";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Switch>
         <Route component={HomePage} path='/'/>
+        <Route component={LoginPage} path='/login'/>
       </Switch>
     </>
   )
